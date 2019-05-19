30e4906f44e0797b74ab785848a57043fbf6050a
Kevin Morland <kmorland@yahoo.com>
2019-05-19 18:11:55 -0400

    Test handler, changes

--- __tests__/handler.test.ts
+++ __tests__/handler.test.ts
@@ -3,12 +3,20 @@ import { Context } from "aws-lambda";
 import {} from "jest";
 //import eventStub from "./stubs/list-user-event.json";
 import { listUsers } from "../handler";
+import * as AWS from "aws-sdk-mock";
 
 // const mockEvent = {} as APIGatewayProxyEvent;
 // const mockContext = {} as Context;
 // const mockCallback = {} as Callback;
 
 describe("User API Tests", () => {
+    beforeAll(() => {
+        AWS.mock("DynamoDB.DocumentClient", "scan", (params, callback) => {
+            console.log(params);
+            console.log(callback);
+        });
+    });
+    
     test("listUsers should return HTTP Status 200", async () => {
         // const response = listUsers(mockEvent, mockContext, mockCallback);
         // expect(response.statusCode).toBe(200);
@@ -18,5 +26,9 @@ describe("User API Tests", () => {
         console.log("result", result);
         expect(result).toMatchSnapshot();
         expect(listUsers).toBeTruthy();
+    });
+
+    afterAll(() => {
+        AWS.restore("DynamoDB.DocumentClient");
     });
 });
