fe476e83f90489957562a218cf253561117adf10
Kevin Morland <kmorland@yahoo.com>
2019-05-19 19:02:19 -0400

    Added test to validate invalid email address check

--- __tests__/handler.test.ts
+++ __tests__/handler.test.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyEvent, Callback, Context } from "aws-lambda";
-import { listUsers } from "../handler";
 import * as AWS from "aws-sdk-mock";
+import { listUsers, getUser } from "../handler";
 
 import createEvent from "aws-event-mocks";
 
@@ -11,19 +11,53 @@ jest.setTimeout(120000);
 
 describe("User API Tests", () => {
     beforeAll(() => {
+        console.log("WTG");
         AWS.mock("DynamoDB.DocumentClient", "scan", (params, callback) => {
-            console.log(params);
-            console.log(callback);
+            console.log("params", params);
+            console.log("callback", callback);
+            callback(null, "got it");
         });
     });
-    
+
     test("listUsers should return HTTP Status 200", async () => {
+        AWS.mock("DynamoDB.DocumentClient", "scan", (params, callback) => {
+            console.log("params", params);
+            console.log("callback", callback);
+            callback(null, "got it");
+        });
         const apiGatewayEvent: APIGatewayProxyEvent = createEvent({
             template: "aws:apiGateway",
         });
 
         const response: any = await listUsers(apiGatewayEvent, mockContext, mockCallback);
         expect(response.statusCode).toBe(200);
+    });
+
+    test("getUser should return 400, invalid email address", async () => {
+        const apiGatewayEvent: APIGatewayProxyEvent = createEvent({
+            template: "aws:apiGateway",
+            merge: {
+                pathParameters: {
+                    email: "kmorland@yahoocom",
+                },
+            },
+        });
+        const response: any = await getUser(apiGatewayEvent, mockContext, mockCallback);
+        expect(response.statusCode).toBe(400);
+    });
+
+    test("getUser body should return 'Email address required, invalid email address'", async () => {
+        const apiGatewayEvent: APIGatewayProxyEvent = createEvent({
+            template: "aws:apiGateway",
+            merge: {
+                pathParameters: {
+                    email: "kmorland@yahoocom",
+                },
+            },
+        });
+        const response: any = await getUser(apiGatewayEvent, mockContext, mockCallback);
+        const { error } = JSON.parse(response.body);
+        expect(error).toBe("Email address required, invalid email address");
     });
 
     afterAll(() => {
